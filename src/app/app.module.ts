880776fa9adbcbd361fae338217455ff5de23ba5
Nicolas Soares Henrique <nicolas.henrique@capgemini.com>
2024-02-28 08:52:07 -0300

    commit provisorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from "./home/home.component";
 import { SharedModule } from "./shared/shared.module";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AboutComponent } from "./about/about.component";
