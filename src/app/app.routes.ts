ddc5c38351f510f566efa4cc3f38720e41acde12
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-07-05 19:52:30 -0300

    [feature] - ajustes diversos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { AppComponent } from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", component: AppComponent },
+	{ path: "**", redirectTo: "", pathMatch: "full" },
+];
